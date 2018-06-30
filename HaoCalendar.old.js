bd7b31679d961d8ecdcd12d61270a747747271c0
Haosedev <530178@qq.com>
2018-06-30 15:20:36 +0800

    first commit

---
+++ HaoCalendar.old.js
@@ -0,0 +1,270 @@
+/*
+ * HaoCalendar
+ * 使用原生代码编写，不需要jquery等支持
+ * Version:0.6.79
+ */
+function HaoCalendar(options) {
+    var opt = {//参数保存在此对象中
+        opts: options,
+        selectDays:[],
+        isShow:false, 
+    };
+    var obj = {
+        //填充日历
+        fillDate: function (year, month) {
+            //本月份第一天是星期几-为显示上个月的天数做铺垫
+            var first_day = new Date(year, month, 1).getDay();
+            //如果刚好是星期天，则空出一行（显示上个月的天数）
+            first_day = first_day == 0 ? 7 : first_day;
+            //本月份最后一天是几号
+            var final_date = new Date(year, month + 1, 0).getDate(),
+            //上个月的最后一天是几号
+                last_date = new Date(year, month, 0).getDate(),
+            //剩余的格子数--即排在末尾的格子数
+                surplus = 42 - first_day - final_date;
+            /*设置表头的日历*/
+            opt.oHeadDate.innerHTML = year + '年' + (month + 1) + '月';
+            /*填充日历执行*/
+            var html = '';
+            //上个月的显示天数
+            for (var i = 0; i < first_day; i++) {
+                var prevDate = year+"-"+(month)+"-"+(last_date - (first_day - 1) + i);
+                if (opt.selectDays.indexOf(prevDate)>=0){
+                    html += '<span class="prev-date-normal calendar-sel">' + (last_date - (first_day - 1) + i) + '</span>';
+                }else{
+                    html += '<span class="prev-date-normal">' + (last_date - (first_day - 1) + i) + '</span>';
+                }
+            }
+            //本月的显示天数
+            for (var j = 0; j < final_date; j++) {
+                var benDate = year+"-"+(month+1)+"-"+(j+1);
+                if (opt.selectDays.indexOf(benDate)>=0){
+                    //console.log("find date："+benDate);
+                    html += '<span class="date-normal calendar-sel">' + (j + 1) + '</span>';
+                }else{
+                    html += '<span class="date-normal">' + (j + 1) + '</span>';
+                }
+            }
+            //下个月的显示天数
+            for (var k = 0; k < surplus; k++) {
+                var nextDate = year+"-"+(month+2)+"-"+(k + 1);
+                if (opt.selectDays.indexOf(nextDate)>=0){
+                    html += '<span class="next-date-normal calendar-sel">' + (k + 1) + '</span>';
+                }else{
+                    html += '<span class="next-date-normal">' + (k + 1) + '</span>';
+                }
+            }
+            //fill
+            opt.oBody.innerHTML = html;
+            // 当天
+            if (year == new Date().getFullYear() && month == new Date().getMonth()) {
+                opt.oBody.getElementsByTagName('span')[first_day + opt.date - 1].className = 'calendar-today date-normal';
+            }
+            //点击赋值ipt得到日期
+            for (var x = 0, v = opt.oBody.getElementsByTagName('span'), len = v.length; x < len; x++) {
+                v[x].onclick = function () {
+                
+                    var now = new Date(year, month, 1), y = 0, m = 0;
+                    if (this.className.indexOf('prev-date-normal') > -1) {
+                        y = new Date(now.setMonth(now.getMonth() - 1)).getFullYear();
+                        m = new Date(now).getMonth();
+                    } else if (this.className.indexOf('next-date-normal') > -1) {
+                        y = new Date(now.setMonth(now.getMonth() + 1)).getFullYear();
+                        m = new Date(now).getMonth();
+                    } else if (this.className.indexOf('date-normal') > -1) {
+                        y = year;
+                        m = month;
+                    }
+                    var date=y + '-' + (m + 1) + '-' + this.innerHTML;
+                    
+                    document.getElementById('date1').value = date;
+                    
+                    //点击标记选中，并写入选中日期列表
+                    var ret={};
+                    var isSelNumber=opt.selectDays.indexOf(date);
+                    ret['operation']='date';
+                    ret['date']=date;
+                    if (opt.selectDays.indexOf(date)>=0){
+                        ret['event']="date-sel";
+                        opt.selectDays.splice(isSelNumber,1);
+                        //移除  并去除class
+                        obj.removeSel(this,"calendar-sel");
+                    }else{
+                        ret['event']="date-normal";
+                        opt.selectDays.push(date);
+                        //新增  并加入class
+                        obj.addSel(this,"calendar-sel");
+                    }
+                    //执行回调函数
+                    opt.opts.callback(ret);
+                    //opt.oWrap.style.display = 'none';//隐藏日历容器
+                }
+            }
+        },
+        // 下个月
+        next: function () {
+            var me = this;
+            opt.oNext.onclick = function () {
+                opt.month++;
+                if (opt.month > 11) {
+                    opt.month = 0;
+                    opt.year++;
+                }
+                // 填充日历
+                me.fillDate(opt.year, opt.month);
+                var ret={};
+                ret['operation']='switch';
+                ret['event']="month-next";
+                ret['date']=opt.year+"-"+opt.month;
+                opt.opts.callback(ret);
+            }
+            
+        },
+        // 上个月
+        prev: function () {
+            var me = this;
+            opt.oPrev.onclick = function () {
+                opt.month--;
+                if (opt.month < 0) {
+                    opt.month = 11;
+                    opt.year--;
+                }
+                // 填充日历
+                me.fillDate(opt.year, opt.month);
+                var ret={};
+                ret['operation']='switch';
+                ret['event']="month-prev";
+                ret['date']=opt.year+"-"+opt.month;
+                opt.opts.callback(ret);
+            }
+
+        },
+        // 下一年
+        nextYear: function () {
+            var me = this;
+            opt.oNextYear.onclick = function () {
+                opt.year++;
+                // 填充日历
+                me.fillDate(opt.year, opt.month);
+                var ret={};
+                ret['operation']='switch';
+                ret['event']="year-next";
+                ret['date']=opt.year+"-"+opt.month;
+                opt.opts.callback(ret);
+            }
+        },
+        // 上一年
+        prevYear: function () {
+            var me = this;
+            opt.oPrevYear.onclick = function () {
+                if (opt.year > 1970) {
+                    opt.year--;
+                }
+                // 填充日历
+                me.fillDate(opt.year, opt.month);
+                var ret={};
+                ret['operation']='switch';
+                ret['event']="year-prev";
+                ret['date']=opt.year+"-"+opt.month;
+                opt.opts.callback(ret);
+            }
+        },
+        //获取元素偏移位置
+        offset: function (ele) {
+            var l = ele.offsetLeft, t = ele.offsetTop, p = ele.offsetParent;
+            while (p) {
+                l += p.offsetLeft + p.clientLeft;
+                t += p.offsetTop + p.clientTop;
+                p = p.offsetParent;
+            }
+            return {left: l, top: t}
+        },
+        //添加Class sel
+        addSel: function (ele, cls){
+            var me = this;
+            if (!me.hasSel(ele, cls)) {
+                ele.className = ele.className == '' ? cls : ele.className + ' ' + cls;
+            }
+        },
+        removeSel: function(ele, cls){
+            var me = this;
+            if (me.hasSel(ele, cls)) {
+                var newClass = ' ' + ele.className.replace(/[\t\r\n]/g, '') + ' ';
+                while (newClass.indexOf(' ' + cls + ' ') >= 0) {
+                    newClass = newClass.replace(' ' + cls + ' ', ' ');
+                }
+                ele.className = newClass.replace(/^\s+|\s+$/g, '');
+            }
+        },
+        hasSel: function(ele, cls){
+            cls = cls || '';
+            if (cls.replace(/\s/g, '').length == 0) return false; //当cls没有参数时，返回false
+            return new RegExp(' ' + cls + ' ').test(' ' + ele.className + ' ');
+        },
+        getSel: function(){
+            return opt.selectDays;
+        },
+        show: function(){
+            opt.isShow=true;
+            opt.oWrap.style.display = 'block';
+        },
+        hide: function(){
+            opt.isShow=false;
+            opt.oWrap.style.display = 'none';
+        },
+        init: function () {//初始化
+            opt.isInit=true;
+            //  初始化参数
+            //创建日历容器固定结构，每次切换日期值即可
+            var Headdiv = document.createElement("DIV");
+            Headdiv.setAttribute('id', opt.opts.wrapId);
+            //<button class="calendar-prev-year">&lt;&lt;</button><button class="calendar-next-year">&gt;&gt;</button>
+            Headdiv.innerHTML = '<div class="calendar-hd">' +
+                    '<div class="calendar-prev">上个月</div><div class="calendar-date"></div>' +
+                    '<div class="calendar-next">下个月</div>' +
+                    '</div><div class="calendar-tit clear">' +
+                    '<span>周日</span><span>周一</span><span>周二</span><span>周三</span><span>周四</span><span>周五</span><span>周六</span>' +
+                    '</div><div class="calendar-main clear"></div>';
+            document.getElementById("calender").appendChild(Headdiv);
+            opt.oWrap = document.getElementById(opt.opts.wrapId);//日历容器
+            opt.trigger = document.getElementById(opt.opts.triggerId);//关联Input容器的
+            opt.oHeadDate = opt.oWrap.getElementsByClassName('calendar-date')[0];//头部日期
+            opt.oBody = opt.oWrap.getElementsByClassName('calendar-main')[0];//日期容器
+            opt.oTit = opt.oWrap.getElementsByClassName('calendar-tit')[0];//星期容器
+            opt.oPrev = opt.oWrap.getElementsByClassName('calendar-prev')[0];//上月按钮
+            opt.oNext = opt.oWrap.getElementsByClassName('calendar-next')[0];//下月按钮
+            //opt.oPrevYear = opt.oWrap.getElementsByClassName('calendar-prev-year')[0];//上月按钮
+            //opt.oNextYear = opt.oWrap.getElementsByClassName('calendar-next-year')[0];//下月按钮
+            opt.year = 0;//年
+            opt.month = 0;//月
+            opt.date = 0;//日
+
+            // 获取今天的日历时间
+            var now = new Date();
+            opt.year = now.getFullYear();
+            opt.month = now.getMonth();
+            opt.date = now.getDate();
+            // 初始化--填充日历
+            this.fillDate(opt.year, opt.month);
+            //切换年月
+            this.next();
+            //this.nextYear();
+            this.prev();
+            //this.prevYear();
+            //设置日历容器位置
+            //opt.oWrap.style.position = 'absolute';            //可以设置跟随Input
+            //opt.oWrap.style.left = this.offset(opt.trigger).left + opt.trigger.offsetWidth + 10 + 'px';
+            //opt.oWrap.style.top = this.offset(opt.trigger).top + 'px';
+            opt.oWrap.style.display = 'none';//默认隐藏日历容器
+            //ipt触发日历选择
+            opt.trigger.onclick = function () {
+                //opt.oWrap.style.display = 'none';   //点击日历后 是否隐藏
+            }
+        }
+    };
+    obj.init();
+    if (opt.opts.show!=false){
+        obj.show();//初始化
+    }
+    return obj;
+}
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# HaoCalendar
-Calendar of Haosedev
+#HaoCalendar
+
+####说明：
+想做一款日历插件的初衷是自己的项目需要在移动端使用一个可以多选（不连续）日期的插件。由于在线上找了众多插件后都不能完全符合项目的要求，要么就是纯E文，Kalendae.js需要改语言，或者类似其他不支持动态宽度，移动端尺寸受手机屏幕影响。要么其他条件符合又不支持多选。反正在尝试过各种插件后发现无论哪款都要修改他们的源码。虽然各作者都非常慷慨的贡献了自己的源码，但是直接修改他们的源文件总是我们作为开发者所不愿意做的。所以打算自己写一个！当然为了发扬节约精神，基础代码也是参考了无私作者的webNick分享的源码和思路。在他的基础上继续调整和开发。现在的版本暂时只为了满足自己项目的需要，但是源码的可读性和注释都非常丰富，只要大家喜欢可以随意修改并据为己有！
+
+###版本
+Version:0.7.83
+
+###使用说明
+####调用配置
+点击日期会有回调回传（并且选中）<br>
+点击换月份按钮会也有回调回传<br>
+也可以使用on方式来绑定方法。
+可以绑定的方法有：<br>
+1. month-next  下个月按钮触发<br>
+2. month-prev  上个月按钮触发<br>
+3. click       点击日期触发<br>
+4. 
+<pre><code>
+    &lt;link rel="stylesheet" type="text/css" href="./HaoCalendar.css" /&gt;
+    &lt;script type="text/javascript" src="./HaoCalendar.js"&gt;&lt;/script&gt;
+    //调用
+    var Cale;
+    Cale=new HaoCalendar({
+        //show:false,             //默认值为true，默认显示
+        wrapId: 'HaoCalendar',  //日历容器id
+        triggerId: 'date1',     //跟随变化的Input ID
+        callback:function(ret){ //回调函数
+            console.log(ret);
+        }
+    });
+    Cale.on('month-next',function(ev){
+        console.log(ev);
+        // 写你的确定之后的逻辑代码...
+    });
+    Cale.on('month-prev',function(ev){
+        console.log(ev);
+        // 写你的确定之后的逻辑代码...
+    });
+    Cale.on('click',function(ev){
+        console.log(ev);
+        // 写你的确定之后的逻辑代码...
+    });  
+    //Cale.show();    //显示，必须先init
+    //Cale.hide();	  //隐藏，必须先init
+</code></pre>
+
